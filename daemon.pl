2888ed5f435c727603fb3d7056f01e684fa0e4a8
Meir Michanie <meirm@riunx.com>
2016-11-30 10:33:00 -0500

    Simplify back daemon script. Simply print child pid to screen

--- daemon.pl
+++ daemon.pl
@@ -3,31 +3,18 @@
 
 use strict;
 use POSIX 'setsid';
-my $WRITEPID=0;
-my $PIDFILE;
-if (@ARGV >= 1){
-	if ($ARGV[0] eq "--pidfile" ){
-		shift @ARGV;
-		($PIDFILE)=shift @ARGV;
-		$WRITEPID=1;
-	}
-}
 
 sub daemonize {
    chdir '/'               or die "Can't chdir to /: $!";
    open STDIN, '/dev/null' or die "Can't read /dev/null: $!";
-   open STDOUT, '>/dev/null'
-						   or die "Can't write to /dev/null: $!";
    defined(my $pid = fork) or die "Can't fork: $!";
    if ($pid){
-	if ($WRITEPID){
-		open (F,">$PIDFILE")|| die($!);
-		print F "$pid\n";
-		close(F);
-	}
-	exit 0;
+		print  "$pid\n";
+	    exit 0;
 	}
    setsid                  or die "Can't start a new session: $!";
+   open STDOUT, '>/dev/null'
+						   or die "Can't write to /dev/null: $!";
    open STDERR, '>&STDOUT' or die "Can't dup stdout: $!";
 }
 
