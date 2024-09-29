1e7f1b9b6912e72b1f833db37fd6ff1eb11f35c1
Meir Michanie <meirm@riunx.com>
2024-09-29 20:38:11 +0200

    Update daemon.pl

--- daemon.pl
+++ daemon.pl
@@ -3,20 +3,28 @@
 
 use strict;
 use POSIX 'setsid';
+use Cwd 'getcwd';
+
+# Cache the original working directory
+my $original_pwd = getcwd();
 
 sub daemonize {
-   chdir '/'               or die "Can't chdir to /: $!";
-   open STDIN, '/dev/null' or die "Can't read /dev/null: $!";
-   defined(my $pid = fork) or die "Can't fork: $!";
-   if ($pid){
-		print  "$pid\n";
-	    exit 0;
-	}
-   setsid                  or die "Can't start a new session: $!";
-   open STDOUT, '>/dev/null'
-						   or die "Can't write to /dev/null: $!";
-   open STDERR, '>&STDOUT' or die "Can't dup stdout: $!";
+    chdir '/'               or die "Can't chdir to /: $!";
+    open STDIN, '/dev/null' or die "Can't read /dev/null: $!";
+    defined(my $pid = fork) or die "Can't fork: $!";
+    if ($pid) {
+        print "$pid\n";
+        exit 0;
+    }
+    setsid                  or die "Can't start a new session: $!";
+    open STDOUT, '>/dev/null'
+                           or die "Can't write to /dev/null: $!";
+    open STDERR, '>&STDOUT' or die "Can't dup stdout: $!";
 }
 
-&daemonize(); 
+&daemonize();
+
+# Change back to the original working directory
+chdir $original_pwd or die "Can't chdir to original PWD: $original_pwd: $!";
+
 exec(@ARGV);
